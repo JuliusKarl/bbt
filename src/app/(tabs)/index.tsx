9017387ffa4e327bd6ef54d7a09714901e42a508
Julius Karl <julius.karl.macrohon@gmail.com>
2025-02-12 23:55:27 +1300

    implement redux toolkit and redux dev tools

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Platform, Button } from 'react-native';
+import { Image, StyleSheet, Platform, Button, Text, View } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
@@ -6,18 +6,33 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 import * as Sentry from "@sentry/react-native";
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@/redux/store/store';
+import { decrement, increment } from '@/redux/slices/counterSlice';
 
 export default function HomeScreen() {
+  const dispatch = useDispatch();
+  const style = styles();
+  const counter = useSelector((state: RootState) => state.counter.value);
+
+  const increaseCounter = () => {
+    dispatch(increment())
+  }
+
+  const decreaseCounter = () => {
+    dispatch(decrement())
+  }
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
           source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+   
         />
       }>
-      <ThemedView style={styles.titleContainer}>
+      {/* <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
       </ThemedView>
@@ -51,22 +66,27 @@ export default function HomeScreen() {
           <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
+      </ThemedView> */}
+      <ThemedView style={style.row}>
+        <Button title="-" onPress={decreaseCounter}/>
+          <Text>{counter}</Text>
+        <Button title="+" onPress={increaseCounter}/>
       </ThemedView>
       <Button title='Throw a Sentry error' onPress={ () => { Sentry.captureException(new Error('First error')) }}/>
     </ParallaxScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
+const styles = () => StyleSheet.create({
+  // titleContainer: {
+  //   flexDirection: 'row',
+  //   alignItems: 'center',
+  //   gap: 8,
+  // },
+  // stepContainer: {
+  //   gap: 8,
+  //   marginBottom: 8,
+  // },
   reactLogo: {
     height: 178,
     width: 290,
@@ -74,4 +94,9 @@ const styles = StyleSheet.create({
     left: 0,
     position: 'absolute',
   },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems:'center'
+  }
 });
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,9 +5,10 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
 import { useColorScheme } from '@/hooks/useColorScheme';
 import * as Sentry from '@sentry/react-native';
+import { store } from '../redux/store/store'
+import { Provider } from 'react-redux'
 
 Sentry.init({
   dsn: 'https://65a10936fdca1657470e34da17e57810@o4508805175902209.ingest.de.sentry.io/4508805178130512',
@@ -36,12 +37,14 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </Provider>
   );
 }
