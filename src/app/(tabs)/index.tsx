466e9b3b202c8a4ad31d9d64766781a664f2d5ef
Julius Karl <julius.karl.macrohon@gmail.com>
2025-02-13 18:47:01 +1300

    setup firebase analytics

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -6,11 +6,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import { RootState, store } from '@/redux/store/store';
 import { decrement, increment } from '@/redux/slices/counterSlice';
 import { getCrashlytics, crash, recordError, } from '@react-native-firebase/crashlytics';
+import { logEvent, getAnalytics } from '@react-native-firebase/analytics';
 
 export default function HomeScreen() {
   const dispatch = useDispatch();
   const style = styles();
   const crashlytics = getCrashlytics();
+  const analytics = getAnalytics();
   const counter = useSelector((state: RootState) => state.counter.value);
 
   // Functions
@@ -45,6 +47,12 @@ export default function HomeScreen() {
       <Button title='Throw a JS crash' onPress={throwJSCrash} />
       <Button title='Throw a Fatal Crashlytics crash' onPress={() => { crash(crashlytics) }} />
       <Button title='Throw a Non-Fatal Crashlytics error' onPress={() => recordError(crashlytics, new Error(), `Test Error: ${JSON.stringify(store.getState())}`)} />
+      <Button
+        title="Log Firebase Analytics Event"
+        onPress={async () =>
+          await logEvent(analytics, 'home_page_event', { value: JSON.stringify(store.getState()) })
+        }
+      />
     </ParallaxScrollView>
   );
 }
